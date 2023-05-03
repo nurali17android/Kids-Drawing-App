07463800a4884edf000624804c51f2987abdf57c
nurik17 <91252221+nurik17@users.noreply.github.com>
2023-05-03 11:46:44 +0600

    finish App

--- app/src/main/java/com/example/drawingapp/MainActivity.kt
+++ app/src/main/java/com/example/drawingapp/MainActivity.kt
@@ -2,11 +2,18 @@ package com.example.drawingapp
 
 import android.annotation.SuppressLint
 import android.app.Dialog
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.provider.MediaStore
 import android.view.View
 import android.widget.ImageButton
+import android.widget.ImageView
 import android.widget.LinearLayout
+import android.widget.Toast
+import androidx.activity.result.ActivityResultLauncher
+import androidx.activity.result.contract.ActivityResultContracts
+import androidx.core.app.ActivityCompat
 import androidx.core.content.ContextCompat
 import androidx.core.view.get
 
@@ -15,6 +22,31 @@ class MainActivity : AppCompatActivity() {
     private var mImageButtonCurrentPaint: ImageButton? = null
 
 
+    val openGalleryLauncher:ActivityResultLauncher<Intent> = registerForActivityResult(ActivityResultContracts.StartActivityForResult()){ result->
+        if (result.resultCode == RESULT_OK && result.data != null){
+            val imageBackground: ImageView = findViewById(R.id.iv_background)
+            imageBackground.setImageURI(result.data?.data)
+        }
+    }
+
+    val requestPermission : ActivityResultLauncher<Array<String>> =
+        registerForActivityResult(ActivityResultContracts.RequestMultiplePermissions()){
+            permissions->
+            permissions.entries.forEach{
+                val permissionName = it.key
+                val isGranted = it.value
+
+                if(isGranted){
+                    Toast.makeText(this, "Permisson granted", Toast.LENGTH_SHORT).show()
+                    val pickIntent = Intent(Intent.ACTION_PICK, MediaStore.Images.Media.EXTERNAL_CONTENT_URI)
+                    openGalleryLauncher.launch(pickIntent)
+                }else{
+                    if(permissionName ==android.Manifest.permission.READ_EXTERNAL_STORAGE){
+                        Toast.makeText(this, "Opps", Toast.LENGTH_SHORT).show()
+                    }
+                }
+            }
+        }
 
     @SuppressLint("MissingInflatedId")
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -28,11 +60,29 @@ class MainActivity : AppCompatActivity() {
         mImageButtonCurrentPaint = linearLayout[2] as ImageButton
         mImageButtonCurrentPaint!!.setImageDrawable(ContextCompat.getDrawable(this,R.drawable.palette_pressed))
 
-        val ib_brush : ImageButton = findViewById(R.id.image)
+        val ib_brush : ImageButton = findViewById(R.id.pal)
         ib_brush.setOnClickListener{
             showBrushSizeDialog()
         }
 
+        val delete : ImageButton = findViewById(R.id.delete)
+        delete.setOnClickListener{
+            drawingView?.onClickUndo()
+        }
+
+        val galleryBtn :ImageButton = findViewById(R.id.gallery)
+        galleryBtn.setOnClickListener {
+            requestStoragePermission()
+        }
+
+    }
+    private fun requestStoragePermission(){
+        if(ActivityCompat.shouldShowRequestPermissionRationale(
+                this,
+                android.Manifest.permission.READ_EXTERNAL_STORAGE)
+        ){
+
+        }
     }
 
     fun paintClicked(view : View){
